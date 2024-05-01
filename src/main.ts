82eeb9836bb19095ac500d7488676bfe8f8f235e
bryanhad <bryanhadinata@gmail.com>
2024-05-01 22:20:58 +0700

    feat: add pdfkit and ejs

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import express from "express"
+import cors from "cors"
+import generatePDF from "./lib/generatePDF"
+import ejs from 'ejs'
+
+const app = express()
+
+// Enable CORS for a specific domain
+const corsOptions = {
+    origin: [
+        "https://your-nextjs-app-domain.com",
+        "http://127.0.0.1:3000",
+        "http://localhost:3000",
+    ],
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello from express!" })
+})
+
+app.get("/generate-pdf", async (req, res) => {
+
+
+        res.setHeader('Content-Disposition', 'attachment; filename="file.pdf"');
+        res.setHeader('Content-Type', 'application/pdf');
+
+        const pdfFile = await generatePDF()
+
+        res.send(Buffer.from(pdfFile));
+
+
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
